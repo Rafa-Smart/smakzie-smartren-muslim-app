4f9463ba269de767e334a31e00b9fd3db60c797c
Rafa Khadafi <rafakhadafi29@gmail.com>
2026-02-16 01:04:06 +0700

    push

--- src/components/CTASection/CTASection.js
+++ src/components/CTASection/CTASection.js
@@ -37,7 +37,7 @@ const CTASection = () => {
 
     // Buat element link untuk download Ziekir+
     const link = document.createElement("a");
-    link.href = `https://github.com/Rafa-Smart/smakzie-smartren-muslim-app/releases/download/v4.0.0/smakzie-muslim.apk`;
+    link.href = `https://github.com/Rafa-Smart/ziekirplus/releases/download/v1.0.0/ziekirplus.apk`;
     link.download = apkName;
     link.target = "_blank";
 
